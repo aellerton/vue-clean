444a794b2d35ad6325807376fc0f8e6b30580c13
Andrew E <andrew.j.ellerton@gmail.com>
2019-02-01 09:28:29 -0800

    minor content changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,14 +12,11 @@
 
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Vue, Typescript, Vuetify, Prettier
         </h1>
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
+          A vue+typescript+vuetify base project that handles linting and fixing
+          formatting cleanly, quickly, correctly and simply.
         </p>
       </v-flex>
 
